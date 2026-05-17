ea528c79e516447b5366db80cc46f3b8b0fc7f8a
Karina Malij <karinamalij0303@gmail.com>
2026-05-17 22:53:16 +0300

    main1

--- main.cpp
+++ main.cpp
@@ -1,20 +1,31 @@
 #include "LLL.h"
+
 #include <iostream>
-#include <vector>
+#include <fstream>
+#include <chrono>
+
 using namespace std;
 
-void printMatrix(const vector<vector<vector<double>>>& mats,
-    const string& filename) {
-
+void printResults(const vector<vector<vector<double>>>& mats, const string& filename) {
     ofstream out(filename);
+    vector<double> deltas ={
+        0.5,
+        0.75,
+        0.90,
+        0.95,
+        0.99
+    };
 
-    for (size_t k = 0; k < mats.size(); k++) {
 
-        out << "Matrix " << k + 1 << "\n";
 
-        for (const auto& row : mats[k]) {
+    for (int i = 0; i < mats.size(); i++) {
+        out << "====================================\n";
+        out << "Matrix " << i + 1 << "\n\n";
+
+        for (const auto& row : mats[i]) {
 
             for (double x : row) {
+
                 out << x << " ";
             }
 
@@ -22,21 +33,41 @@ void printMatrix(const vector<vector<vector<double>>>& mats,
         }
 
         out << "\n";
+
+        for (double delta : deltas) {
+
+            vector<vector<double>> B = mats[i];
+
+            auto start = chrono::high_resolution_clock::now();
+
+            LLLResult res = LLL(B, delta);
+
+            auto end = chrono::high_resolution_clock::now();
+            double time_ms = chrono::duration<double, milli> (end - start).count();
+            double H = Hadamard(B);
+            double b1_norm = vectorNorm(B[0]);
+
+
+            out << "delta=" << delta << "\n";
+            out << "swaps=" << res.swaps << "\n";
+            out << "H(B)=" << H << "\n";
+            out << "b1_norm=" << b1_norm << "\n";
+            out << "time_ms=" << time_ms << "\n\n";
+            out << "\n";
+        }
     }
 
     out.close();
 }
 
 
-int main() {
 
+int main() {
     auto mats = generateMatrix();
 
     cout << "Generated matrices: " << mats.size() << endl;
-
-    printMatrix(mats, "matrices.txt");
-
-    cout << "Saved to matrices.txt" << endl;
+    printResults(mats, "results.txt");
+    cout << "Saved to results.txt" << endl;
 
     return 0;
 }
